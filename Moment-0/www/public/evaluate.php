06cf13898ac4723537b0c24f2d07bf1d6fd3512c
Linus Torstensson <linus.torstensson@live.se>
2025-08-28 12:58:01 +0200

    5 frågor eval

--- Moment-0/www/public/evaluate.php
+++ Moment-0/www/public/evaluate.php
@@ -8,14 +8,22 @@
      	$name = $_POST['name'];
         $ans1 = $_POST['q1'];
      	$ans2 = $_POST['q2'];
+		$ans3 = $_POST['q3'];
+		$ans4 = $_POST['q4'];
+		$ans5 = $_POST['q5'];
      	$points = 0;
      	
      	if($ans1 == 9)
            $points++;
      	if($ans2 == 15)
            $points++;
-     	
-     	echo("<p>" . $name . "fick " . $points . " av 2 möjliga</p>");
+     	if($ans3 == 28)
+           $points++;
+     	if($ans4 == 5)
+           $points++;
+     	if($ans5 == 4)
+           $points++;
+     	echo("<p>" . $name . "fick " . $points . " av 5 möjliga</p>");
 ?>
 </body>
 </html>
